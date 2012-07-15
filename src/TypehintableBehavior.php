f25c294216b97af8cad7e721c088f095987f8476
William DURAND <william.durand1@gmail.com>
2012-07-15 16:00:55 +0200

    Fix doc

--- src/TypehintableBehavior.php
+++ src/TypehintableBehavior.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the Propel package.
+ * This file is part of the TypehintableBehavior package.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
@@ -9,8 +9,7 @@
  */
 
 /**
- * @author     William Durand <william.durand1@gmail.com>
- * @package    propel.generator.behavior
+ * @author William Durand <william.durand1@gmail.com>
  */
 class TypehintableBehavior extends Behavior
 {
